8c68e3ba2d76b4aaa8af12438a8f483fb4468518
GHOST <miles.naberhaus@gmail.com>
2021-11-05 15:55:33 -0600

    Fixed naming conventions

--- link_3/simulation_3.py
+++ link_3/simulation_3.py
@@ -12,7 +12,7 @@ from rprint import print
 
 ## configuration parameters
 router_queue_size = 0  # 0 means unlimited
-simulation_time = 20  # give the network sufficient time to transfer all packets before quitting
+simulation_time = 10  # give the network sufficient time to transfer all packets before quitting
 
 if __name__ == '__main__':
 	object_L = []  # keeps track of objects, so we can kill their threads
@@ -21,16 +21,17 @@ if __name__ == '__main__':
 	client_1 = network_3.Host(1)
 	object_L.append(client_1)
 	client_2 = network_3.Host(2)
-	object_L.append(client_1)
+	object_L.append(client_2)
 	server_1 = network_3.Host(3)
 	object_L.append(server_1)
 	server_2 = network_3.Host(4)
+	object_L.append(server_2)
 
 	# routers with routing tables built in based on destination
 	router_a = network_3.Router(name='A', intf_count=2, max_queue_size=router_queue_size, rt={0: 0, 1: 1})
-	router_b = network_3.Router(name='B', intf_count=1, max_queue_size=router_queue_size, rt={0: 0})
-	router_c = network_3.Router(name='C', intf_count=1, max_queue_size=router_queue_size, rt={0: 0})
-	router_d = network_3.Router(name='D', intf_count=2, max_queue_size=router_queue_size, rt={3: 0, 4: 1})
+	router_b = network_3.Router(name='B', intf_count=2, max_queue_size=router_queue_size, rt={0: 0})
+	router_c = network_3.Router(name='C', intf_count=2, max_queue_size=router_queue_size, rt={0: 0})
+	router_d = network_3.Router(name='D', intf_count=2, max_queue_size=router_queue_size, rt={3: 0, 4: 0})
 	object_L.append(router_a)
 	object_L.append(router_b)
 	object_L.append(router_c)
@@ -45,7 +46,7 @@ if __name__ == '__main__':
 	link_layer.add_link(link_3.Link(client_1, 0, router_a, 0, 30))  # Host1 - RouterA
 	link_layer.add_link(link_3.Link(client_2, 0, router_a, 1, 30))  # Host2 - RouterA
 	link_layer.add_link(link_3.Link(router_a, 0, router_b, 0, 30))  # RouterA - RouterB
-	link_layer.add_link(link_3.Link(router_a, 1, router_c, 0, 30))  # RouterA - RouterC
+	link_layer.add_link(link_3.Link(router_a, 1, router_c, 1, 30))  # RouterA - RouterC
 	link_layer.add_link(link_3.Link(router_b, 0, router_d, 0, 30))  # RouterB - RouterD
 	link_layer.add_link(link_3.Link(router_c, 0, router_d, 1, 30))  # RouterC - RouterD
 	link_layer.add_link(link_3.Link(router_d, 0, server_1, 0, 30))  # RouterD - Host3
@@ -59,12 +60,6 @@ if __name__ == '__main__':
 	# create some send events
 	client_1.udt_send(3,
 					"From c 1 to s 1",
-					link_layer.link_L[0].in_intf.mtu)
-	client_1.udt_send(4,
-					"From c 1 to s 2",
-					link_layer.link_L[0].in_intf.mtu)
-	client_2.udt_send(3,
-					"From c 2 to s 1",
 					link_layer.link_L[0].in_intf.mtu)
 	client_2.udt_send(4,
 					"From c 2 to s 2",
