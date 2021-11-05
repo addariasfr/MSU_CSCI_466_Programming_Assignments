699c9c89f9295a2740f06df1ff3de3552a73e3b9
GHOST <miles.naberhaus@gmail.com>
2021-11-05 14:20:11 -0600

    Edited functionality of recieving servers

--- link_3/simulation_3.py
+++ link_3/simulation_3.py
@@ -26,11 +26,11 @@ if __name__ == '__main__':
 	object_L.append(server_1)
 	server_2 = network_3.Host(4)
 
-	# routers with routing tables built in (source: destination)
+	# routers with routing tables built in based on destination
 	router_a = network_3.Router(name='A', intf_count=2, max_queue_size=router_queue_size, rt={0: 0, 1: 1})
 	router_b = network_3.Router(name='B', intf_count=1, max_queue_size=router_queue_size, rt={0: 0})
 	router_c = network_3.Router(name='C', intf_count=1, max_queue_size=router_queue_size, rt={0: 0})
-	router_d = network_3.Router(name='D', intf_count=2, max_queue_size=router_queue_size, rt={2: 0, 3: 1})
+	router_d = network_3.Router(name='D', intf_count=2, max_queue_size=router_queue_size, rt={3: 0, 4: 1})
 	object_L.append(router_a)
 	object_L.append(router_b)
 	object_L.append(router_c)
@@ -57,18 +57,18 @@ if __name__ == '__main__':
 		t.start()
 
 	# create some send events
-	client_1.udt_send(2,
-					  "From c 1 to s 1",
-					  link_layer.link_L[0].in_intf.mtu)
 	client_1.udt_send(3,
-					  "From c 1 to s 2",
-					  link_layer.link_L[0].in_intf.mtu)
-	client_2.udt_send(2,
-					  "From c 2 to s 1",
-					  link_layer.link_L[0].in_intf.mtu)
+					"From c 1 to s 1",
+					link_layer.link_L[0].in_intf.mtu)
+	client_1.udt_send(4,
+					"From c 1 to s 2",
+					link_layer.link_L[0].in_intf.mtu)
 	client_2.udt_send(3,
-					  "From c 2 to s 2",
-					  link_layer.link_L[0].in_intf.mtu)
+					"From c 2 to s 1",
+					link_layer.link_L[0].in_intf.mtu)
+	client_2.udt_send(4,
+					"From c 2 to s 2",
+					link_layer.link_L[0].in_intf.mtu)
 
 	# give the network sufficient time to transfer all packets before quitting
 	sleep(simulation_time)
